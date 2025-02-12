c8d331ea882f69c4a98d336ddad1099195248ddc
Praisearu <praisearu@gmail.com>
2025-02-11 18:30:45 -0600

    Adding Books to library

---
+++ src/library/management/LibraryApp.java
@@ -0,0 +1,34 @@
+package library.management;
+
+public class LibraryApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Library lib = new Library();
+		//using the parameterized method
+		Book b1 = new Book("The Cat in the Hat", "Dr.Seuss", "9780007247882", 8.99 );
+		lib.addBook(b1);
+		
+		Book b2 = new Book( "Verity", "Colleen Hoover", "9781538724736", 10.99);
+		lib.addBook(b2);
+		
+		//using the setter method
+		Book b3 = new Book(); 
+		b3.setTitle("The 48 Laws of Power");
+		b3.setAuthor("Robert Greene");
+		b3.setISBN("9788467039054");
+		b3.setPrice(14.55);
+		
+		//copying book 2 ; so b4 and b2 are the same 
+		Book b4 = new Book(b2);
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
