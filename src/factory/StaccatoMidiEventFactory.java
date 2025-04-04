4a3ae370baf6b7b0977aecba1e1f36f617561653
EnobongOffiong <edoffiong@gmail.com>
2025-04-03 23:05:27 -0500

    Updated StaccatoMidiEventFactory to implement MidiEventFactory and implements it's methods

--- src/factory/StaccatoMidiEventFactory.java
+++ src/factory/StaccatoMidiEventFactory.java
@@ -1,33 +1,17 @@
 package factory;
 import myLittleMozart.midi.MidiEventData;
-//import javax.sound.midi.*;
+import javax.sound.midi.*;
 
-public class StaccatoMidiEventFactory {
-    public MidiEventData Staccato(MidiEventData e) {
-        // If this is a NoteOff event
-        if (e.getNoteOnOff() == 0) {
-            int oldTick = e.getStartEndTick();
-            int newTick = oldTick - 120; // Make it end earlier
-            
-            // Prevent negative tick values differently
-            if (newTick < 0) {
-                newTick = 0;
-            }
-            
-            // Create a new NoteOff event that ends earlier
-            MidiEventData staccatoEvent = new MidiEventData(
-                newTick,
-                e.getNoteOnOff(), // Keep it as NoteOff (0)
-                e.getChannel(),
-                e.getNote(),
-                e.getVelocity(),
-                e.getInstrument()
-            );
-            
-            return staccatoEvent;
-        }
-        
-        // If it's a NoteOn event, return it unchanged
-        return e;
+public class StaccatoMidiEventFactory implements MidiEventFactory {
+    public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+        ShortMessage msg = new ShortMessage();
+        msg.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        return new MidiEvent(msg, tick);
+    }
+
+    public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+        ShortMessage msg = new ShortMessage();
+        msg.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        return new MidiEvent(msg, tick - 120); //staccato subtracts 120 ticks
     }
 }
