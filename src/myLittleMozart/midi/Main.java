7855423678a55ea0a310504c1a94a9dff3513869
EnobongOffiong <edoffiong@gmail.com>
2025-04-03 23:41:01 -0500

    Implemented Main.java class

--- src/myLittleMozart/midi/Main.java
+++ src/myLittleMozart/midi/Main.java
@@ -4,9 +4,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        MidiCsvParser parser = new MidiCsvParser();
-        List<MidiEventData> events = parser.parseCsv();
+        try {
+            List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./files/mystery_song.csv");
+            Sequence sequence = new Sequence(Sequence.PPQ, 384);
+            Track track = sequence.createTrack();
 
-//   
+            MidiEventFactoryAbstract factoryAbstract = new LegatoMidiEventFactoryAbstract();
+            MidiEventFactory factory = factoryAbstract.createFactory();
+
+            InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy();
+            instrumentStrategy.applyInstrument(track, 0);
+            instrumentStrategy = new TrumpetStrategy();
+            instrumentStrategy.applyInstrument(track, 1);
+
+            PitchStrategy pitchStrategy = new HigherPitchStrategy();
+
+            for (MidiEventData event : midiEvents) {
+                int modifiedNote = pitchStrategy.modifyPitch(event.getNote());
+                if (event.getNoteOnOff() == ShortMessage.NOTE_ON) {
+                    track.add(factory.createNoteOn(event.getStartEndTick(), modifiedNote, event.getVelocity(), event.getChannel()));
+                } else {
+                    track.add(factory.createNoteOff(event.getStartEndTick(), modifiedNote, event.getChannel()));
+                }
+            }
+
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.setSequence(sequence);
+            sequencer.start();
+
+            while (sequencer.isRunning()) {
+                Thread.sleep(100);
+            }
+            Thread.sleep(500);
+            sequencer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
