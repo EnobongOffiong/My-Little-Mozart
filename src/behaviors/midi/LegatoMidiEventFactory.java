84cf0bae8c2d4ae1a7766db1824ef1e48a7f24bf
Praisearu <praisearu@gmail.com>
2025-04-03 19:24:59 -0500

    Implement StaccatoMidiEventFactory and LegatoMidiEventFactory

---
+++ src/behaviors/midi/LegatoMidiEventFactory.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package behaviors.midi;
+
+import myLittleMozart.midi.MidiEventData;
+import myLittleMozart.midi.MidiEvent;
+
+
+/**
+ * create note events with longer durations and minimal gaps between the NoteOff and the next NoteOn event.
+ */
+public class LegatoMidiEventFactory{
+	
+	public MidiEventData legato(MidiEventData e) {
+		
+		int originalTick = e.getStartEndTick();
+	    
+	    
+	    int newTick = originalTick + 80; 
+	    
+	    
+	    MidiEventData legatoEvent = new MidiEventData(
+	        newTick,  // Adjusted duration
+	        e.getNoteOnOff(),  // Note on/off status (same as original)
+	        e.getChannel(),    // MIDI channel
+	        e.getNote(),       // MIDI note
+	        e.getVelocity(),   // Velocity (same as original)
+	        e.getInstrument()  // Instrument (same as original)
+	    );
+	    
+	    return legatoEvent;
+	}
+		
+		
+}
+
